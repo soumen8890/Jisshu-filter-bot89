76957cb84a03efa2edfa77d5ef0bb2cf7b2e910f
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-07-06 21:59:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Spportgroup89')
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviemashala89')
 
 # Verification
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/tutorial990")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/tutorial990")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/tutorial990")
